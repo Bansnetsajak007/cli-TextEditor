7c24ec22f2e5aae6708b6d34546e025a137b17d0
sajak <sajakbasnet77@gmail.com>
2024-07-30 11:23:28 +0545

    added test pyscripts

--- main.go
+++ main.go
@@ -1,73 +1,3 @@
-/*
-	can't edit every line can only appded content in file
-
-*/
-
-/*
-		V1
-package main
-
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("USage: go run main.go <filename>")
-		return
-	}
-
-	filename := os.Args[1]
-	content , err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Println("Creating new file:", filename)
-		content = []byte{}
-	}
-
-	//splits the file content into individual lines.
-	lines := strings.Split(string(content), "\n")
-
-	fmt.Println("CLI Text Editor")
-	fmt.Println("Commands: :w (save), :q (quit), :wq (save and quit)")
-
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		for i, line := range lines {
-			fmt.Printf("%d: %s\n", i+1, line)
-		}
-		fmt.Print("> ")
-		scanner.Scan()
-		input := scanner.Text()
-
-		switch input {
-		case ":q":
-			return
-		case ":w":
-			saveFile(filename, lines)
-		case ":wq":
-			saveFile(filename, lines)
-			return
-		default:
-			lines = append(lines, input)
-		}
-	}
-}
-
-func saveFile(filename string, lines []string) {
-	content := strings.Join(lines, "\n")
-	err := os.WriteFile(filename, []byte(content), 0644)
-	if err != nil {
-		fmt.Println("Error saving file:", err)
-	} else {
-		fmt.Println("File saved successfully")
-	}
-}
-
-*/
 
 package main
 
@@ -96,7 +26,7 @@ func NewEditor(filename string) *Editor {
 	return &Editor{filename: filename, lines: lines, currentLine: 0}
 }
 
-//method on struct
+//method on struct method vanaya special type of functions
 func (e * Editor) display() {
 	fmt.Print("\033[2J")  // Clear screen
 	fmt.Print("\033[H")   // Move cursor to top-left corner
@@ -161,7 +91,7 @@ func (e *Editor) saveFile(){
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: go run editor.go <filename>")
-		return	
+		return
 	}
 
 	editor := NewEditor(os.Args[1])
