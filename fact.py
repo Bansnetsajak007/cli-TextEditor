8df6f27cdaa3d1ccd5239fc254268cb4a279b528
sajak <sajakbasnet77@gmail.com>
2024-07-30 15:43:06 +0545

    refactored files structure

--- fact.py
+++
@@ -1,9 +0,0 @@
-def fact(n):
-    if n==1 or n==0:
-	    return n
-    else:
-	    return n*fact(n-1)
-    
-n = int(input("Enter any number"))
-res = fact(n)
-print(res)
--- temp.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-
-sum = num1 + num2
-
-print (sum)
-def mul(a,b):
-    return a*b
